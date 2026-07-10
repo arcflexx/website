c574bde5057a8d789807d38155cb3c4c3256bacb
sally <sally@sallys-MacBook-Pro.local>
2026-07-09 19:28:56 -0500

    a

--- lib/shopify/client.ts
+++ lib/shopify/client.ts
@@ -4,11 +4,11 @@ export const OVERRIDE_STOREFRONT_API_URL = "https://uujwhv-c7.myshopify.com/api/
 export const OVERRIDE_REQUEST_HEADERS = {
   "Content-Type": "application/json",
   "Accept": "application/json",
-  "X-Shopify-Storefront-Access-Token": process.env.NEXT_PUBLIC_STOREFRONT_API_TOKEN,
+  "X-Shopify-Storefront-Access-Token": process.env.NEXT_PUBLIC_STOREFRONT_API_TOKEN || "",
 }
 
 export const client = createStorefrontClient({
   // load environment variables according to your framework and runtime
-  storeDomain: process.env.NEXT_PRIVATE_SHOPIFY_STORE_DOMAIN || process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN || "",
+  storeDomain: process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN,
   publicStorefrontToken: process.env.NEXT_PUBLIC_STOREFRONT_API_TOKEN,
 });
